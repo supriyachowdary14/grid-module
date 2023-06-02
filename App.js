78bbb14312c9d855cb09fdfd1f4c8bb2f0af70fe
supriyachowdary14 <124381152+supriyachowdary14@users.noreply.github.com>
2023-06-02 18:45:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Cards from "./components/Cards/Cards";
+import Footer from "./components/Footer/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Cards />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
